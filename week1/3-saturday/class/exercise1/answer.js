c2c53fe247d19224bd2c83f49f0be9e59e6de726
Matthew Day <matt@matthewday.net>
2020-07-18 12:33:41 -0400

    Add class exercises for Saturday Week 1

---
+++ week1/3-saturday/class/exercise1/answer.js
@@ -0,0 +1,34 @@
+function lvl1exercise1 () {
+  // Declare a variable without instantiating it and return it!
+
+}
+
+function lvl1exercise2 () {
+  // Declare and instantiate a number and return it
+
+}
+
+function lvl1exercise3 () {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+
+}
+
+function lvl1exercise4 () {
+  // Declare and instantiate a string "Hello World!" and return it
+
+}
+
+function lvl1exercise5 () {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+
+}
+
+function lvl1exercise6 () {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+
+}
+
+function lvl1exercise7 () {
+  // Declare and instantiate a boolean value 'false' and return it
+
+}
