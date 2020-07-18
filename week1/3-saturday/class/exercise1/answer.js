083f1c191c512f5da19574a3f927df06abf226ef
Stacey Graham <stacey.graham1106@gmail.com>
2020-07-18 13:15:51 -0400

    add exercise answers

--- week1/3-saturday/class/exercise1/answer.js
+++ week1/3-saturday/class/exercise1/answer.js
@@ -1,34 +1,83 @@
 function lvl1exercise1 () {
   // Declare a variable without instantiating it and return it!
 
-}
+  var name;
+  console.log('Stacey');
 
-function lvl1exercise2 () {
-  // Declare and instantiate a number and return it
-
-}
-
-function lvl1exercise3 () {
-  // Declare and instantiate a floating point number that is not a whole number and return it
+  return name;
 
 }
 
-function lvl1exercise4 () {
-  // Declare and instantiate a string "Hello World!" and return it
+lvl1exercise1 ();
+
+
+ function lvl1exercise2 () {
+//   // Declare and instantiate a number and return it
+var num = 5;
+console.log(num);
+
+return num;
+}
+
+lvl1exercise2();
+// 
+
+ function lvl1exercise3 () {
+//   // Declare and instantiate a floating point number that is not a whole number and return it
+var number = 4.1;
+
+console.log(number);
+
+return number;
+
+}
+lvl1exercise3();
+}
+
+ function lvl1exercise4 () {
+//   // Declare and instantiate a string "Hello World!" and return it
+var string = "Hello World!";
+console.log(string);
+
+return string;
 
 }
 
-function lvl1exercise5 () {
-  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+lvl1exercise4();
+// }
 
+ function lvl1exercise5 () {
+//   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+var array = ["Hello World!", 4];
+console.log(array)
+  return array;
 }
 
-function lvl1exercise6 () {
-  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+lvl1exercise5(1);
+// }
 
+ function lvl1exercise6 () {
+//   // Declare and instantiate an object containing the key-value pairs 
+//key1 -> "Hello World!" and key2 -> 4, and return it
+var myObject = {
+  greeting: "Hello World!",
+  number: 4,
+  
+};
+console.log(myObject.greeting, myObject.number);
 }
+
+
+lvl1exercise6();
+// }
 
 function lvl1exercise7 () {
-  // Declare and instantiate a boolean value 'false' and return it
+//   // Declare and instantiate a boolean value 'false' and return it
+var colors = false;
+   console.log(colors);
+  colors = colors && false;
+   return colors;
+   }
 
-}
+ lvl1exercise7(); 
+// // }
