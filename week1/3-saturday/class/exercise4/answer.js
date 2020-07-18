e93059b46d08bd08d7638785e1a267ffc602c486
JohnLoftin <jgloftin@gmail.com>
2020-07-18 12:46:46 -0400

    Exercise 4 comppleted

--- week1/3-saturday/class/exercise4/answer.js
+++ week1/3-saturday/class/exercise4/answer.js
@@ -1,29 +1,38 @@
 function lvl4exercise1 () {
   // Return the boolean value "true"
-
+var apple = true;
+return apple;
 }
-
+console.log(lvl4exercise1());
 function lvl4exercise2 () {
   // Return the boolean value "false"
-
+var orange = !orange;
+return orange;
 }
-
+console.log(lvl4exercise2());
 function lvl4exercise3 (bool) {
   // Return the opposite of the input boolean value
-
+  var newish = !bool;
+  return newish;
 }
-
+console.log(lvl4exercise3(true));
 function lvl4exercise4 (bool1, bool2) {
   // Return the logical "and" of the input boolean values
-
+  var test4 = bool1 && bool2
+  console.log(test4);
 }
-
+lvl4exercise4(true,false);
 function lvl4exercise5 (bool1, bool2) {
-  // Return the logical "or" of the input boolean values
-
+  // Return the logical "or" of the input boolean values  
+  var test5 = bool1 || bool2
+ console.log(test5);
 }
-
+lvl4exercise5(true,false);
 function lvl4exercise6 (bool1, bool2) {
   // Return the logical "equivalence" of the input boolean values
-
+  var test6 = bool1 === bool2
+  console.log(test6);
 }
+lvl4exercise6(true,false);
+
+
