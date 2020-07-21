9159d031145cabde33e70153ff1e4effdbece87d
Rudy Saavedra <RudyCruisin@outlook.com>
2020-07-20 22:49:59 -0400

    exercise 7

---
+++ week1/3-saturday/homework/exercise7/answer.js
@@ -0,0 +1,7 @@
+// Print a Banner
+function printBanner(text) {
+    console.log("*".repeat(text.length+4));
+    console.log("* " + text +" *");
+    console.log("*".repeat(text.length+4));
+}
+printBanner("Welcome to DigitalCrafts");
