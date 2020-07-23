ca4b349810e5b3d9fc9ef5a04666bcb14af4d66e
Rudy Saavedra <RudyCruisin@outlook.com>
2020-07-23 18:35:56 -0400

    changes to for loop

--- week1/3-saturday/homework/exercise8/answer.js
+++ week1/3-saturday/homework/exercise8/answer.js
@@ -1,11 +1,11 @@
 // // Factor a Number
 function factors(num) {
     var numarr = [];
-    for (i = 0; i <= num ; i++) {
+    for (i = 1; i <= num ; i++) {
         if (num % i === 0) {
             numarr.push(i);
         }
     }
     console.log(numarr);
 }
-factors(144);
+factors(122);
