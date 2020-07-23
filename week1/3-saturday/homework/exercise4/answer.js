feb753c01ef60a60ed6cd911c5b23a0b41339394
Rudy Saavedra <RudyCruisin@outlook.com>
2020-07-22 21:29:36 -0400

    updated to the correct solution

--- week1/3-saturday/homework/exercise4/answer.js
+++ week1/3-saturday/homework/exercise4/answer.js
@@ -1,7 +1,16 @@
-// //Madlib
+// Print Numbers
 
-function madlib (name, car) {
-    console.log(name + " drives a " + car + "!");
+function printNumbers(x, y) {
+    for (i = x; i <= y; i++) {
+        console.log(i);
+    }
 }
+printNumbers(1, 10);
 
-madlib("Rudy","Corvette");
+function printNumbers2(x, y) {
+    while (y >= x) {
+        console.log(x);
+        x++;
+    }
+}
+printNumbers2(40, 44);
