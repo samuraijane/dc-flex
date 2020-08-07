956ed899faf6fcc194f446b4c29860d30371b611
Matthew Day <matt@matthewday.net>
2020-08-07 11:39:40 -0400

    Add homework exercises for Thursday

---
+++ week5/2-thursday/homework/exercise5/script.js
@@ -0,0 +1,3 @@
+// Add a box to "boxContainer" everytime you click the button
+// HINT: Look up createElement(), appendChild()
+// HINT HINT: You can add the "box" class to elements like so: https://www.w3schools.com/howto/howto_js_add_class.asp
