1c77bb9e02386b96464aed8fb256abe557ef829f
Stacey Graham <stacey.graham1106@gmail.com>
2020-08-11 06:51:33 -0400

    adding answer to exercise3

--- week5/2-thursday/homework/exercise3/script.js
+++ week5/2-thursday/homework/exercise3/script.js
@@ -1,7 +1,9 @@
 // Make the box blue when you click the button. (Here's some code to help you get started)
 
 var button = document.getElementById("myButton");
+const box = document.getElementById("box");
 
 button.addEventListener('click', function(){
-    
+    box.style.backgroundColor = "blue";
+  
 })
