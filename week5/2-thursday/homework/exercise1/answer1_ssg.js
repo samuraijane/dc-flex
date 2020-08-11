ef4d8ebc0de104035364d1f06ce07ce071e48bc9
Stacey Graham <stacey.graham1106@gmail.com>
2020-08-11 00:02:15 -0400

    added answer to exercise1

---
+++ week5/2-thursday/homework/exercise1/answer1_ssg.js
@@ -0,0 +1,8 @@
+/**************************
+    Stacey Graham:
+    Week5 - JSDOM Exercise1
+**************************/
+
+// Change the text of the h1 tag to "I have changed the text"
+const heading = document.getElementById('#title');
+heading.innerHTML = 'I have changed the text';
