8aad836173ae3b00b06dd9d127f7610253e46206
Stacey Graham <stacey.graham1106@gmail.com>
2020-08-10 23:55:53 -0400

    assigning document.getElementById(box) to a variable

--- week5/2-thursday/homework/exercise2/script.js
+++ week5/2-thursday/homework/exercise2/script.js
@@ -1,2 +1,3 @@
 // Change the background color of the box to blue
-document.getElementById("box").style.backgroundColor = "blue";
+const box = document.getElementById("box");
+box.style.backgroundColor = "blue";
