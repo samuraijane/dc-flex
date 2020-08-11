cb0a646a3c2d8e3a202e93bc3ecd5ca69fdc9b12
Stacey Graham <stacey.graham1106@gmail.com>
2020-08-11 17:33:18 -0400

    added my solution to exercise4

--- week5/2-thursday/homework/exercise4/script.js
+++ week5/2-thursday/homework/exercise4/script.js
@@ -1,2 +1,12 @@
 // Make all of the boxes blue when you click the button
 // Notice that all the boxes have "class" instead of "id"
+
+const button = document.getElementById('myButton');
+
+box.addEventListener('click', function() {
+    const boxes = document.getElementsByClassName("box");
+    for (let box of boxes) {
+        box.style.background = "blue";
+    }
+  
+});
