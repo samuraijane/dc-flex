eb5de7e77a466366660a3092e4ad9e10e89f276d
bensvarnum <bens.varnum@gmail.com>
2020-08-15 12:43:57 -0400

    add code to sign-in

--- week5/1-tuesday/class/exercise1/groupA/sign-in.js
+++ week5/1-tuesday/class/exercise1/groupA/sign-in.js
@@ -1,10 +1,10 @@
 let form = document.getElementById("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(e);
+  //   console.log(e);
 
   const data = new FormData(form);
-  console.log(stringifyFormData(data));
+  //   console.log(stringifyFormData(data));
 });
 
 function stringifyFormData(fd) {
@@ -14,3 +14,24 @@ function stringifyFormData(fd) {
   }
   return JSON.stringify(data, null, 4);
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data2 = new FormData(e.target);
+  console.log(data2);
+  const stringified = stringifyFormData(data2);
+  console.log(stringified);
+  postData(stringified).then((data2) =>
+    console.log(
+      `Your form has been submitted with the following data.\n\n${data2}`
+    )
+  );
+});
+const postData = (data2) => {
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      res(data2);
+    }, 1500);
+  });
+  return promise;
+};
