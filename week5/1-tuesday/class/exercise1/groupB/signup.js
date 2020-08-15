24a1bb3e62c319c3e8f8940c26aecb218eeddc1e
Robert Powell <r.powelliv@gmail.com>
2020-08-15 11:57:59 -0400

    signuppromise

--- week5/1-tuesday/class/exercise1/groupB/signup.js
+++ week5/1-tuesday/class/exercise1/groupB/signup.js
@@ -5,6 +5,8 @@ form.addEventListener("submit", function(e){
     console.log(e);
     const data = new FormData(form);
     console.log(stringifyFormData(data));
+    const stringified = stringifyFormData(data);
+    postData(stringified).then(data => console.log(`${data} Your form has been submitted`))
 });
 
 function stringifyFormData(fd) {
@@ -14,3 +16,20 @@ function stringifyFormData(fd) {
     }
     return JSON.stringify(data,null,4);
 };
+
+
+// const handleSubmit = e => {
+//     e.preventDefault();
+//     const data = new FormData(e.target);
+//     const stringified = stringifyFormData(data);
+//     postData(stringified).then(data => console.log(`${data} Your form has been submitted`));
+// }
+
+const postData = (data) => {
+    const promise = new Promise((res, rej) => {
+        setTimeout(() => {
+            res('Success!');
+        }, 1500);
+    });
+    return promise;
+}
