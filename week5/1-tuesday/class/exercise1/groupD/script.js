e266f7d3a53790761f5b4d0f82ce0e22db42357c
JohnLoftin <jgloftin@gmail.com>
2020-08-15 12:19:19 -0400

    updated js to use Promise

--- week5/1-tuesday/class/exercise1/groupD/script.js
+++ week5/1-tuesday/class/exercise1/groupD/script.js
@@ -3,7 +3,8 @@ const form = document.getElementById("contact-form");
 form.addEventListener('submit', function(e){
     e.preventDefault();
     const data = new FormData(form);
-    console.log(stringifyFormData(data));
+    const stringified = stringifyFormData(data);
+    postData(stringified).then(data => console.log(`Your form has been submitted with the following data.\n\n${data}`));
 });
 
 function stringifyFormData(fd){
@@ -12,4 +13,13 @@ function stringifyFormData(fd){
         data[key] = fd.get(key);
     }
     return JSON.stringify(data, null, 4);
+}
+
+const postData = (data) => {
+    const promise = new Promise((res, rej) => {
+        setTimeout(() => {
+            res(data);
+        }, 1500);
+    });
+    return promise;
 }
