7ddeffd83b04bf05c1a7e38bf4f2562ab455223c
Daniel Gallagher <danielbgallagher@gmail.com>
2020-08-15 14:02:36 -0400

    spinny thing

--- week5/1-tuesday/class/exercise1/groupC/script.js
+++ week5/1-tuesday/class/exercise1/groupC/script.js
@@ -15,13 +15,16 @@ function stringifyFormData(fd){
 const handleSubmit = e => {
     e.preventDefault();
     submit.disabled = true;
+    loading();
     const data = new FormData(e.target);
     const stringified = stringifyFormData(data);
     //console.log(stringified);
     postData(stringified).then(data => {
         submit.disabled = false;
+        removeLoading();
         console.log(`Your form has been submitted with the following data.\n\n${data}`)
     });
+
 }
 
   const postData = (data) => {
@@ -33,14 +36,32 @@ const handleSubmit = e => {
     });
     return promise;
   };
-
-
-
   var popupSpin = document.createElement("div");
-  popupSpin.appendChild("main");
+  var main = document.getElementsByClassName("main")[0];
+  
+
+function loading (){
+  main.appendChild(popupSpin)
   popupSpin.style.position = "fixed";
 
   var spin = document.createElement('img');
-  spin.appendChild("popupSpin");
-  spin.src = "throbber_12.gif"
-  
+  popupSpin.appendChild(spin);
+  spin.src = "img/throbber_12.gif"
+
+  popupSpin.style.minHeight = "100vh";
+  popupSpin.style.width = "100%";
+
+  popupSpin.style.display = "flex";
+  popupSpin.style.justifyContent = "center";
+  popupSpin.style.alignItems = "center";
+  popupSpin.style.zIndex = "999";
+  popupSpin.style.top = "0";
+  popupSpin.style.left ="0";
+  popupSpin.style.backgroundColor = "rgba(150,150,150,.8)";
+} 
+
+function removeLoading(){
+
+main.removeChild(popupSpin);
+
+}
