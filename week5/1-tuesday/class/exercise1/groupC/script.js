d3b8e73238505f4049ecbe9b6f9d73da9a1f68d1
Alexandra Kasper <alexandrakasper@Alis-MacBook-Pro.local>
2020-08-15 12:58:34 -0400

    disable button

--- week5/1-tuesday/class/exercise1/groupC/script.js
+++ week5/1-tuesday/class/exercise1/groupC/script.js
@@ -1,13 +1,7 @@
 var form = document.getElementById("form");
+var submit = document.getElementById("submit");
 
-form.addEventListener('submit' ,function(e) {
-    e.preventDefault();
-    console.log(e);
-    const data = new FormData(form);
-    console.log(stringifyFormData(data));
-    
-});
-
+form.addEventListener('submit', (e) => handleSubmit(e));
 
 
 function stringifyFormData(fd){
@@ -17,3 +11,25 @@ function stringifyFormData(fd){
     }
     return JSON.stringify(data, null, 4);
 }
+
+const handleSubmit = e => {
+    e.preventDefault();
+    submit.disabled = true;
+    const data = new FormData(e.target);
+    const stringified = stringifyFormData(data);
+    //console.log(stringified);
+    postData(stringified).then(data => {
+        submit.disabled = false;
+        console.log(`Your form has been submitted with the following data.\n\n${data}`)
+    });
+}
+
+  const postData = (data) => {
+    console.log(data);
+    const promise = new Promise((res, rej) => {
+      setTimeout(() => {
+        res(data);
+      }, 1500);
+    });
+    return promise;
+  };
