631327301de4504ee59c56a2ee61c46ae59ba805
Robert Powell <r.powelliv@gmail.com>
2020-08-15 12:35:07 -0400

    signuppromise

--- week5/1-tuesday/class/exercise1/groupB/signup.js
+++ week5/1-tuesday/class/exercise1/groupB/signup.js
@@ -2,11 +2,16 @@ let form = document.getElementById("formcoookie");
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
+    document.getElementById("submit-button").disabled=true;
     console.log(e);
     const data = new FormData(form);
     console.log(stringifyFormData(data));
     const stringified = stringifyFormData(data);
-    postData(stringified).then(data => console.log(`Your form has been submitted with the following data.\n\n${data}`))
+    postData(stringified).then(data => {
+        document.getElementById("submit-button").disabled=false;
+        console.log(`Your form has been submitted with the following data.\n\n${data}`)
+    }
+);
 });
 
 function stringifyFormData(fd) {
